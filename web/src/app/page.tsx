9f9287c1da869cc300405b4960ffbb81babe56a5
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-09-01 22:55:48 -0300

    web
    first commit 🚀

---
+++ web/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50 ">
+      <div className="w-[420px] text-gray-200 text-[40px] font-bold leading-[50px]">
+        Sua cápsula do tempo
+      </div>
+    </div>
+  )
+}
