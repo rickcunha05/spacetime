ce1a0c92a84f908197d4babdd299e9932f6d55c3
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-09-03 01:54:32 -0300

    criação do crud de memorias 🎉🎉

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,15 @@
-import fastify from 'fastify'
+import { fastify } from 'fastify'
 import { PrismaClient } from '@prisma/client'
+import { memoriesRoutes } from './Routes/memories'
+import cors from '@fastify/cors'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', () => {
-  const users = prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
 
+app.register(memoriesRoutes)
 app
   .listen({
     port: 3333,
