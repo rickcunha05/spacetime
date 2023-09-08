0db6c7e99621605ab02f4bd0e62c3bc192c8383a
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2023-09-08 02:49:24 -0300

    screen de nova memoria finalizada

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,14 +1,72 @@
-import { View, Text, Switch, TextInput, ScrollView } from 'react-native'
+import { View, Text, Switch, TextInput, ScrollView, Image } from 'react-native'
 import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 import Icon from '@expo/vector-icons/Feather'
-import { Link } from 'expo-router'
+import { Link, useRouter } from 'expo-router'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { useState } from 'react'
+import { api } from '../src/assets/lib/api'
 
 export default function NewMemory() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
+
   const [isPublic, setIsPublic] = useState(false)
+  const [content, setContent] = useState('')
+  const [preview, setPreview] = useState<string | null>(null)
+
+  async function OpenImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      // deu erro e eu não tratou
+    }
+  }
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        // no native o FormData não tem a propriedade set, então usamos a append
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          // no android ele não consegue acessar o FormData direto então vocÊ precisa infromar um Content-Type
+        },
+      })
+      coverUrl = uploadResponse.data.fileUrl
+    }
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+    router.push('/memories')
+  }
   return (
     <ScrollView
       className="flex-1 px-8"
@@ -36,23 +94,35 @@ export default function NewMemory() {
         </View>
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={OpenImagePicker}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2 ">
-            <Icon name="image" color="#FFF" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            // eslint-disable-next-line jsx-a11y/alt-text
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+            />
+          ) : (
+            <View className="flex-row items-center gap-2 ">
+              <Icon name="image" color="#FFF" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
           className="p=0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
         <TouchableOpacity
           activeOpacity={0.7}
+          onPress={handleCreateMemory}
           className="items-center self-end rounded-full bg-green-500 px-5 py-2"
         >
           <Text className="font-alt text-sm uppercase text-black">Salvar</Text>
